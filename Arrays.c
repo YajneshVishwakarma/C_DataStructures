a4e8b900ea2b632036803e8e269edaf3ce7ff6c5
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-05-21 17:47:08 +0530

    Update Arrays.c

--- Arrays.c
+++ Arrays.c
@@ -11,91 +11,7 @@ void swap(int* arr,int start,int end)
     arr[end] = temp;
 }
 
-int partion(int arr[],int lb,int hb)
-{
-    int pivot = arr[lb],start = lb,end = hb;
 
-    while(start < end)
-    {
-        while(arr[start] <= pivot)
-        {
-            start++;
-        }
-
-        while(arr[end] > pivot)
-        {
-            end--;
-        }
-
-        if(start < end)
-        {
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-        }
-
-    }
-
-        temp = arr[lb];
-        arr[lb] = arr[end];
-        arr[end] = temp;
-        return end;
-
-    
-}
-
-void quickSort(int arr[],int lb,int ub)
-{
-    if(lb < ub)
-    {
-        pos = partion(arr,lb,ub);
-        quickSort(arr,lb,pos-1);
-        quickSort(arr,pos+1,ub);
-    }
-    
-}
-
-void selectionSort(int arr[])
-{
-    printf("Sorting the array using Selection sort\n");
-    for(i=0;i<size;i++)
-    { 
-        min = i;
-
-        for(j=i+1;j<size;j++)
-        {
-            if(arr[j] < arr[min])
-            {
-                min = j;
-            }
-        }
-
-        if(min!=i)
-        {
-            temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-        }
-    }
-}
-
-void insertionSort(int arr[])
-{
-    printf("Sorting the array using Insertion sort\n");
-    for(i = 0; i<size ; i++)
-    {
-        temp = arr[i];
-        j= i-1;
-
-        while(j>=0 && arr[j]>temp)
-        {
-            arr[j+1] = arr[j];
-            j--;
-        }
-
-        arr[j+1] = temp;
-    }
-}
 
 void insertion(int pos)
 {
@@ -134,79 +50,7 @@ void insertion(int pos)
     }
 }
 
-void linearSearch(int temp)
-{
-    printf("\nSearching the array using Linear search\n");
-    scanf("%d",&temp);
-    
-    for(j=0;j<size;j++)
-    {
-        if(temp == arr[j])
-        {
-            flag=1;
-            printf("The element found in the index %d\n",j);
-        }
-    }
-    if(flag == 0)
-    {
-        printf("Not found\n");
-    }
-}
 
-void binarySearch(int temp)
-{
-    printf("\nSearching the array using Binary search\n");
-    int l=0,r=size-1,mid;
-    
-    printf("\nEnter the element to search\n");
-    scanf("%d",&temp);
-    while(l<=r)
-    {
-        mid=(l+r)/2;
-        
-        if(temp == arr[mid])
-        {
-            printf("Element found\n");
-            flag=1;
-            break;
-        }
-        else if(arr[mid]>temp)
-        {
-            l=mid+1;
-        }
-        else if(arr[mid]<temp)
-        {
-            r=mid-1;
-        }
-    }
-    if(flag==0)
-    {
-        printf("Not found\n");
-    }
-}
-
-void bubbleSort()
-{
-    printf("\nSorting the array using Bubble sort\n");
-    for(i=0;i<size-1;i++)
-    {
-        flag = 0;
-        for(j=0;j<size-1-i;j++)
-        {
-            if(arr[j]>arr[j+1])
-            {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                flag = 1;
-            }
-        }
-        if(flag == 0)
-        {
-            break;
-        }
-    }
-}
 
 void arrayDeletion( int pos)
 {
@@ -261,12 +105,6 @@ int main()
     
     printf("\n");
 
-    printf("Sorting elements using Quick sort");
-    quickSort(arr,0,size-1);
-
-    printArray(arr);
-        
-    
     
     // insertion(pos);
 
